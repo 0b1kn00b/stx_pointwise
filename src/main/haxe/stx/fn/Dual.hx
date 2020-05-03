101a23c2e82590c484da7ce4a11f3a9bf5f1f911
dayz <polysemantic@gmail.com>
2020-05-03 10:00:47 +0200

    fixed type declarations in Dual

--- src/main/haxe/stx/fn/Dual.hx
+++ src/main/haxe/stx/fn/Dual.hx
@@ -3,9 +3,9 @@ package stx.fn;
 @:using(stx.fn.Dual.DualLift)
 @:forward @:callable abstract Dual<Pi,Pii,Ri,Rii>(DualDef<Pi,Pii,Ri,Rii>) from DualDef<Pi,Pii,Ri,Rii> to DualDef<Pi,Pii,Ri,Rii>{
   static public var _(default,never) = DualLift;
-  static public function unit<P>():Dual<P,P,P,P>{
+  @:noUsing static public function unit<Pi,Pii>():Dual<Pi,Pii,Pi,Pii>{
     return new Dual(
-      (tp:Couple<P,P>) -> (tp:Couple<P,P>)
+      (tp:Couple<Pi,Pii>) -> (tp:Couple<Pi,Pii>)
     );
   }
   public function new(self:DualDef<Pi,Pii,Ri,Rii>){
@@ -22,16 +22,16 @@ class DualLift{
   static public function then<Pi,Pii,Ri,Rii,Riii>(self:Dual<Pi,Pii,Ri,Rii>,then:Couple<Ri,Rii>->Riii):Unary<Couple<Pi,Pii>,Riii>{
     return Unary._.then(self,then);
   }
-  static public function into<Pi,Pii,Ri,Rii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Ri->Rii->Rii){
-    return self.then(__.decouple(fn));
+  static public function into<Pi,Pii,Ri,Rii,Riii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Ri->Rii->Riii):Unary<Couple<Pi,Pii>,Riii>{
+    return then(self,__.decouple(fn));
   }
   static public function pass<Pi,Pii,Ri,Ri0,Rii0>(self:Dual<Pi,Pii,Ri0,Rii0>,fn:Ri0->Rii0->Couple<Ri0,Rii0>):Dual<Pi,Pii,Ri0,Rii0>{
     return self.then(__.decouple(fn));
   }
-  static public function first<Pi,Pii,Ri,Rii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Ri->Rii):Dual<Pi,Pii,Rii,Rii>{
+  static public function first<Pi,Pii,Ri,Rii,Riii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Ri->Riii):Dual<Pi,Pii,Riii,Rii>{
     return self.then(fn.fn().first());
   }
-  static public function second<Pi,Pii,Ri,Rii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Rii->Rii):Dual<Pi,Pii,Ri,Rii>{
+  static public function second<Pi,Pii,Ri,Rii,Riii>(self:Dual<Pi,Pii,Ri,Rii>,fn:Rii->Riii):Dual<Pi,Pii,Ri,Riii>{
     return self.then(fn.fn().second());
   }
 }
